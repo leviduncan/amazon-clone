a3dda6162cfb34a4359ef104d4b26d5f9eea0ed0
Darrin Duncan <darrin@darrinduncan.com>
2020-11-23 22:34:34 -0500

    Added Nav, Home and Product components

---
+++ src/Product.js
@@ -0,0 +1,13 @@
+import "./Product.css"
+
+import React from 'react'
+
+function Product() {
+    return (
+        <div className="product__box">
+            <p>The Lean Startup - Book</p>
+        </div>
+    )
+}
+
+export default Product
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDcByMogfvHjsv8QVrifF-0XLYw2vscBDk",
+    authDomain: "challenge-f5756.firebaseapp.com",
+    databaseURL: "https://challenge-f5756.firebaseio.com",
+    projectId: "challenge-f5756",
+    storageBucket: "challenge-f5756.appspot.com",
+    messagingSenderId: "308479919672",
+    appId: "1:308479919672:web:f826074e8b1a4815e488cd",
+    measurementId: "G-2P0YZJ0MCP"
+  };
