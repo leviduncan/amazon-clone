dc9bcdd737dc2136cacd4cb8d7e902ae63586495
Darrin Duncan <darrin@darrinduncan.com>
2020-11-24 12:47:09 -0500

    Css Changes

--- src/Product.js
+++ src/Product.js
@@ -1,13 +1,22 @@
 import "./Product.css"
 
 import React from 'react'
+import StarRateIcon from '@material-ui/icons/StarRate';
 
 function Product() {
     return (
-        <div className="product__box">
-            <p>The Lean Startup - Book</p>
+      <div className="product">
+        <div className="product__info">
+          <p>The lean Startup</p>
+          <p className="product__price"><small>$</small><strong>19.69</strong></p>
+          <div className="product__rating">
+            <StarRateIcon />
+          </div>
         </div>
-    )
+        <img src="https://m.media-amazon.com/images/I/51T-sMqSMiL._AC_SL260_.jpg" alt="The Lean Startup" />
+       <button>Add to cart button</button> 
+      </div>
+    );
 }
 
 export default Product
